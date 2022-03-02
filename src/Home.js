ed6f43e04a94e9e2e1d3853f45ef414138b18418
Jason Yapri <jason@inergi.id>
2022-03-02 14:50:57 +0700

    lesson-7

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,19 @@
 const Home = () => {
-  return ( 
-    <h2>Homepage</h2>
+
+  const handleClick = (e) => {
+    console.warn('handleClick', e);
+  }
+
+  const handleClickAgain = (name, e)=> {
+    console.log('hello ' + name, e)
+  }
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => {handleClickAgain('Jason', e)}}>Click me again</button>
+    </div>
   );
 }
 
