84724e17edb59ca3c53ed9ce64316c9ba2d674f8
Jason Yapri <jason@inergi.id>
2022-03-03 11:30:08 +0700

    lesson-14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,11 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
+
+  useEffect(() => {
+    console.log('use effect ran')
+    console.log(blogs)
+  });
 
   return (
     <div className="home">
