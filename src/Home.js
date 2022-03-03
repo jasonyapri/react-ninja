1a8eba5c7390355b4598b53e1a52ededcb5d0639
Jason Yapri <jason@inergi.id>
2022-03-03 11:38:29 +0700

    lesson-15

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     setBlogs(newBlogs);
   };
 
+  const [name, setName] = useState('mario');
+
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -16,13 +18,15 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran')
-    console.log(blogs)
-  });
+    console.log(name)
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
       {/* <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" /> */}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
