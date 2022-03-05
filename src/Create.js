3310036131e0aa257681674e21aee2c02c896b09
Jason Yapri <jason@inergi.id>
2022-03-05 13:09:24 +0700

    lesson-30

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,9 +21,11 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log('new blog added');
-      clearForm();
+      // clearForm();
     }).finally(() => {
       setIsPending(false);
+
+      navigate('/');
     })
   }
 
