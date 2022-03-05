c6df2bc3e945231f31151765bfe58499e99f46fd
Jason Yapri <jason@inergi.id>
2022-03-05 13:01:32 +0700

    lesson-29

--- src/Create.js
+++ src/Create.js
@@ -5,12 +5,30 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      clearForm();
+    }).finally(() => {
+      setIsPending(false);
+    })
+  }
+
+  const clearForm = () => {
+    setTitle('');
+    setBody('');
+    setAuthor('mario');
   }
 
   return ( 
@@ -38,7 +56,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        { !isPending && <button>Add blog</button> }
+        { isPending && <button disabled>Adding blog...</button> }
       </form>
     </div>
   );
